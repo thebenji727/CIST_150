05f5706a0166d41e0b573f1a3773c631df2369be
thebenji727 <thedragonking727@gmail.com>
2024-03-20 16:17:09 -0400

    online class work 3/20

--- week 11/dynamic_programming.py
+++ week 11/dynamic_programming.py
@@ -10,3 +10,18 @@
 ## as number of elements in a recursion go up the number of times the function is called exponentially increases
 ## Different types of time complexity: ex. the quicksort function we did in recursions
 # time complexities we want to achieve are linear or less
+
+# steps of dynamic programming:
+#       1) break down the complex problem into simpler solutions
+#       2) find optimal solutions to the sub problems
+#       3) store the results of the sub-problems - memoization
+#       4) resue the results so that a given sub-problem isn't calculated more than once
+#       5) calculate the result of the complex problem
+## These are aplicable to problems which have:
+#       Overlapping sub-problems
+#       final solution can be obtained by combining optimal solutions of sub-problems
+# Dynamic programming is better used for opimization problems (min. possible time or use min, possible memory space)
+
+
+# Longest Common Subsequence (LCS)
+#
--- week 11/fibonacci.py
+++ week 11/fibonacci.py
@@ -16,7 +16,7 @@ for num in [5, 10, 15, 20, 25, 30, 35, 40]:
     print(f"Total function calls: {rec_call_count:,}")
     print("*"*500)
     print()
-'''
+
 
 ## Memoization solution
 
@@ -44,3 +44,83 @@ for num in [5, 10, 15, 20, 25, 30, 35, 40]:
     print(f"Total function calls: {rec_call_count:,}")
     print("*"*500)
     print()
+
+
+
+# Iteration solution
+ite_call_count = 0
+
+def fib_val_it(fib_num):
+    global  ite_call_count
+    ite_call_count += 1
+    if fib_num <= 1:
+        return 1
+    else:
+        fib_list = [1, 1]
+        for i in range(2, fib_num):
+            fib_list.append(fib_list[i-1]+ fib_list[i-2])
+        return fib_list[-1]
+
+print(fib_val_it(10))
+
+
+###LCS_RECURSION
+def lcs_rec (A, B, i, j):
+    if len(A) == 0 or len(B) == 0 or A[i] == '\0' or B[j] == '\0':
+        #return 0
+        return ""
+    elif A[i] == B[j]:
+        #return 1 + lcs_rec(A, B, i+1, j+1)
+        return A[i] + lcs_rec(A, B, i+1, j+1)
+    else:
+        #return max(lcs_rec(A, B, i+1, j), lcs_rec(A, B, i, j+1))
+        return max(lcs_rec(A, B, i+1, j), lcs_rec(A, B, i, j+1), key=len)
+
+str1 = "bd"
+str2 = "abcd"
+
+str1 = str1 + "\0"
+str2 = str2 + "\0"
+
+for _ in range(len(str1)):
+
+
+#seq_len = lcs_rec(A=str1,B=str2,i=0,j=0)
+#print(seq_len)
+
+lcs_char = lcs_rec(A=str1, B=str2, i=0, j=0)
+print(lcs_char)
+
+# the #ed out code prints the NUMBER of similar charaters in the string
+'''
+
+# LCS_MEMOIZATION
+def lcs_memoization(A, B, i, j):
+    global lcs_memo
+    global calls
+    calls += 1
+    if len(A) == 0 or len(B) == 0 or A[i] == '\0' or B[j] == '\0':
+        return 0
+    if lcs_memo[i][j] == " ":
+        if A[i] == B[j]:
+            lcs_memo[i][j] = 1 + lcs_memoization(A, B, i+1, j+1)
+        else:
+            lcs_memo[i][j] = max(lcs_memoization(A, B, i+1, j), lcs_memoization(A, B, i, j+1))
+    return lcs_memo[i][j]
+
+calls = 0
+str1 = "bd"
+str2 = "abcd"
+
+str1 = str1 + "\0"
+str2 = str2 + "\0"
+
+lcs_memo = []
+
+for _ in range(len(str1)):
+    lcs_memo.append([" "] * len(str2))
+
+print(lcs_memo)
+seq_len = lcs_memoization(A=str1, B=str2, i=0, j=0)
+print(seq_len)
+print(calls)
